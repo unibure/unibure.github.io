a1da2f8facad2c175505f6e5877fed8ca0057529
unibure <unibure@gmail.com>
2023-09-16 17:13:00 +0900

    commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>unibure portfollio</title>
+
+  <!-- font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;600;700&family=Roboto:wght@100&display=swap" rel="stylesheet">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="./assets/css.css">
+
+  <!-- JS -->
+  <script src="./assets/js.js" defer></script>
+  
+</head>
+<body>
+  <section class="header">
+    <div class="top_head">
+      unibure Portfolio
+    </div>
+    <ul class="gnb_nav">
+      <li class="gnb_btn">About</li>
+      <li class="gnb_btn">WebSite</li>
+      <li class="gnb_btn">Mobile</li>
+      <li class="gnb_btn">Javascript</li>
+    </ul>
+  </section>
+
+</body>
+</html>
